457e6c6c823bd2767f184e46056061191a9384ab
Amir Azizafshari <55924754+amir002001@users.noreply.github.com>
2023-05-08 19:29:27 -0400

    feat: iterate through all meal cards and click and print

--- main.go
+++ main.go
@@ -4,7 +4,22 @@ import (
 	"fmt"
 
 	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/proto"
 )
+
+type Meal struct {
+	Name         string
+	ImageUrl     string
+	Ingredients  string
+	Calories     int
+	Protein      int
+	Fat          int
+	SaturatedFat int
+	Carbs        int
+	Sugar        int
+	Cholestrol   int
+	Sodium       int
+}
 
 func main() {
 	page := rod.New().MustConnect().MustPage("https://app.yumba.ca/#/on-the-menu")
@@ -14,6 +29,28 @@ func main() {
 		panic(1)
 	}
 	for _, mealCard := range mealCards {
-		fmt.Println(mealCard.Text())
+		span, err := mealCard.Element("span")
+		if err != nil {
+			panic(1)
+		}
+		if err := span.Click(proto.InputMouseButtonLeft, 1); err != nil {
+			panic(1)
+		}
+		modal, err := page.Element(".meal-modal")
+		for err != nil {
+			modal, err = page.Element(".meal-modal")
+		}
+		fmt.Println(modal.Text())
+		if err != nil {
+			panic(1)
+		}
+		fmt.Println(modal.Text())
+		closeBtn, err := modal.Element(".close-modal")
+		if err != nil {
+			panic(1)
+		}
+		if err := closeBtn.Click(proto.InputMouseButtonLeft, 1); err != nil {
+			panic(1)
+		}
 	}
 }
